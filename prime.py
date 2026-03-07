118089dc3e508e73a49b1964bf6aa2ed22cc82c6
juliethrodri24-web <juliethrodri.24@gmail.com>
2026-03-07 20:54:10 +0100

    ejercicio pylint prime

--- prime.py
+++ prime.py
@@ -1,13 +1,13 @@
 import math
 
 def is_prime(n):
-
+    '''calcula los numeros primos del 1 al 100'''
     if n <= 1:
         return False
     for i in range(2, int(math.sqrt(n) + 1)):
         if n % i == 0:
             return False
-        return True
+    return True
 
 def main():
 
@@ -15,8 +15,9 @@ def main():
 
     for i in range(100):
         if is_prime(i):
-            print (i, end='')
+            print (i, end=' ')
     print()
 
 if __name__ == '__main__':
     main()
+    
