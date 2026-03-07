46a611610db48b06020eaff81d5ef0f609ecee2f
juliethrodri24-web <juliethrodri.24@gmail.com>
2026-03-07 18:50:33 +0100

    edicion final de lanzar una moneda

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
+import pandas as pd
 import scipy.stats
 import streamlit as st
 import time
-import pandas
 
+# estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
 if 'experiment_no' not in st.session_state:
     st.session_state['experiment_no'] = 0
+
 if 'df_experiment_results' not in st.session_state:
-    st.session_state['df_experiment_results'] = pd.DataFrame(columns['no','iteraciones','media'])
-    
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
 
 st.header('Lanzar una moneda')
 
 chart = st.line_chart([0.5])
 
-def toss_coin(n): # función que emula el lanzamiento de una moneda
+def toss_coin(n):
 
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
@@ -36,6 +37,7 @@ start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
     st.session_state['df_experiment_results'] = pd.concat([
         st.session_state['df_experiment_results'],
