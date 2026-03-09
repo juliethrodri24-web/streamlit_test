41c724bd1d21533c966807dac8f28d14f82cbdc0
juliethrodri24-web <juliethrodri.24@gmail.com>
2026-03-09 17:34:51 +0100

    ejercicio test_age

--- age_group.py
+++ age_group.py
@@ -1,42 +1,31 @@
 def get_age_group(age):
- 
-     """
-     Devuelve el grupo de edad basado en la edad en años dentro del intervalo 0..150
-     de lo contrario, devuelve 'desconocido'.
-     """
- 
-     if 0 <= age <= 14:
-         return 'infancia'
-     elif 15 <= age <= 24:
-         return 'juventud'
-     elif 25 <= age <= 64:
-         return 'adulto'
-     elif 65 <= age <= 80: 
-         return 'vejez'
-     
-         # Completa esta función para que pase la prueba unitaria
-     else:
-         return 'desconocido'
- 
- def test_get_age_group():
- 
-     """prueba unitaria para get_age_group"""
- 
-     assert get_age_group(-1) == 'desconocido'
-     assert get_age_group(0) == 'infancia'
-     assert get_age_group(14) == 'infancia'
-     assert get_age_group(15) == 'juventud'
-     assert get_age_group(24) == 'juventud'
-     assert get_age_group(25) == 'adulto'
-     assert get_age_group(64) == 'adulto'
-     assert get_age_group(65) == 'vejez'
-     assert get_age_group(80) == 'vejez'
-     assert get_age_group(150) == 'desconocido'
-     print("todas las pruebas pasaron con exito!")
-if __name__ == "__main__"
+    """
+    Devuelve el grupo de edad basado en la edad en años.
+    """
+    if 0 <= age <= 14:
+        return 'infancia'
+    elif 15 <= age <= 24:
+        return 'juventud'
+    elif 25 <= age <= 64:
+        return 'adulto'
+    elif 65 <= age <= 80: 
+        return 'vejez'
+    else:
+        return 'desconocido'
+
+def test_get_age_group():
+    """Prueba unitaria para get_age_group"""
+    assert get_age_group(-1) == 'desconocido'
+    assert get_age_group(0) == 'infancia'
+    assert get_age_group(14) == 'infancia'
+    assert get_age_group(15) == 'juventud'
+    assert get_age_group(24) == 'juventud'
+    assert get_age_group(25) == 'adulto'
+    assert get_age_group(64) == 'adulto'
+    assert get_age_group(65) == 'vejez'
+    assert get_age_group(80) == 'vejez'
+    assert get_age_group(150) == 'desconocido'
+    print("¡Todas las pruebas pasaron con éxito!")
+
+if __name__ == "__main__":
     test_get_age_group()
-
-     
-    
- 
-
